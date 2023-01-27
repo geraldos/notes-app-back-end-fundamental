a0f0691654fed4eaaf1acbd6e91bf2959890fd8e
geraldo <geraldo.sepdwijaya@maybank.co.id>
2023-01-27 16:04:38 +0700

    user registration implemented

--- migrations/1674577216304_create-table-notes.js
+++ migrations/1674577216304_create-table-notes.js
@@ -34,3 +34,44 @@ exports.up = pgm => {
 exports.down = pgm => {
   pgm.dropTable('notes');
 };
+
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined
+
+exports.up = pgm => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true
+    },
+    title: {
+      type: 'VARCHAR(50)',
+      notNull: true
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true
+    },
+    performer: {
+      type: 'VARCHAR(30)',
+      notNull: true
+    },
+    genre: {
+      type: 'VARCHAR(30)',
+      notNull: true
+    },
+    duration: {
+      type: 'INTEGER'
+    },
+    albumId: {
+      type: 'VARCHAR(50)'
+    }
+  })
+}
+
+exports.down = pgm => {
+  pgm.dropTable('songs')
+}
+
+
